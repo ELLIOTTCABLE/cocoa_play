8c7656c2228a8eae4ed1a299c018c43fba806563
elliottcable <git@elliottcable.name>
2008-12-18 22:45:11 -0900

    Time to create our own classes!

---
+++ lottery/LotteryEntry.h
@@ -0,0 +1,16 @@
+//
+//  LotteryEntry.h
+//  lottery
+//
+//  Created by elliottcable on 12/18/08.
+//  Copyright 2008 yreality. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface LotteryEntry : NSObject {
+
+}
+
+@end
---
+++ lottery/LotteryEntry.m
@@ -0,0 +1,14 @@
+//
+//  LotteryEntry.m
+//  lottery
+//
+//  Created by elliottcable on 12/18/08.
+//  Copyright 2008 yreality. All rights reserved.
+//
+
+#import "LotteryEntry.h"
+
+
+@implementation LotteryEntry
+
+@end
