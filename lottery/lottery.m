ca154e2d22dbdf2a74031718035771a603e02043
elliottcable <git@elliottcable.name>
2008-12-18 22:44:51 -0900

    The next step is just to iterate over some stuff

--- lottery/lottery.m
+++ lottery/lottery.m
@@ -1,10 +1,19 @@
 #import <Foundation/Foundation.h>
 
-int main (int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-    [pool drain];
-    return 0;
+int main (int argc, const char *argv[]) {
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  
+  NSMutableArray *array = [[NSMutableArray alloc] init];
+  for (int i=0;i<10;i++) {
+    NSNumber *newNumber = [[NSNumber alloc] initWithInt:(i * 3)];
+    [array addObject:newNumber];
+  }
+  
+  for (int i=0;i<10;i++) {
+    NSNumber *numberToPrint = [array objectAtIndex:i];
+    NSLog(@"The number at index %d is %@",  i, numberToPrint);
+  }
+  
+  [pool drain];
+  return 0;
 }
