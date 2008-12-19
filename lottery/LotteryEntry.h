16dbf2a3614bccde3a923b74151bfea10e525f81
elliottcable <git@elliottcable.name>
2008-12-18 22:47:38 -0900

    Implementation

--- lottery/LotteryEntry.h
+++ lottery/LotteryEntry.h
@@ -1,16 +1,13 @@
-//
-//  LotteryEntry.h
-//  lottery
-//
-//  Created by elliottcable on 12/18/08.
-//  Copyright 2008 yreality. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
-
+#import <Foundation/Foundation.h>
 
 @interface LotteryEntry : NSObject {
-
+  NSCalendarDate *entryDate;
+  int firstNumber;
+  int secondNumber;
 }
-
+- (void)prepareRandomNumbers;
+- (void)setEntryDate:(NSCalendarDate *)date;
+- (NSCalendarDate *)entryDate;
+- (int)firstNumber;
+- (int)secondNumber;
 @end
--- lottery/LotteryEntry.m
+++ lottery/LotteryEntry.m
@@ -1,14 +1,26 @@
-//
-//  LotteryEntry.m
-//  lottery
-//
-//  Created by elliottcable on 12/18/08.
-//  Copyright 2008 yreality. All rights reserved.
-//
-
 #import "LotteryEntry.h"
 
-
 @implementation LotteryEntry
+
+- (void)prepareRandomNumbers {
+  firstNumber = (random() % 100) + 1;
+  secondNumber = (random() % 100) + 1;
+}
+
+- (void)setEntryDate:(NSCalendarDate *)date {
+  entryDate = date;
+}
+
+- (NSCalendarDate *)entryDate {
+  return entryDate;
+}
+
+- (int)firstNumber {
+  return firstNumber;
+}
+
+- (int)secondNumber {
+  return secondNumber;
+}
 
 @end
