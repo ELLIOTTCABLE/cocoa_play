665a46de7a51b9a625751de9d65a56c2e9e04032
elliottcable <git@elliottcable.name>
2008-12-19 12:11:23 -0900

    Starting chapter 5! Adding framework "SpeakLine" application.

---
+++ SpeakLine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpeakLine
+//
+//  Created by elliottcable on 12/19/08.
+//  Copyright yreality 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
