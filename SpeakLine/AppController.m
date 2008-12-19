f51d644de85a5e780a1ad2a02d0fd8b47ba9aace
elliottcable <git@elliottcable.name>
2008-12-19 13:40:42 -0900

    Added application logic.

--- SpeakLine/AppController.m
+++ SpeakLine/AppController.m
@@ -1,14 +1,32 @@
-//
-//  AppController.m
-//  SpeakLine
-//
-//  Created by elliottcable on 12/19/08.
-//  Copyright 2008 yreality. All rights reserved.
-//
-
 #import "AppController.h"
 
-
 @implementation AppController
+
+- (id)init {
+  [super init];
+  
+  // Logs can help the beginner understand what is happening and hunt down bugs.
+  NSLog(@"init");
+  
+  // Create a new instance of NSSpeechSynthesizer with the default voice.
+  speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+  return self;
+}
+
+- (IBAction)sayIt:(id)sender {
+  NSString *string = [textField stringValue];
+  
+  if ([string length] == 0) {
+    NSLog(@"string from %@ is of zero-length", textField);
+    return;
+  }
+  [speechSynth startSpeakingString:string];
+  NSLog(@"Have started to say: %@", string);
+}
+
+- (IBAction)stopIt:(id)sender {
+  NSLog(@"stopping");
+  [speechSynth stopSpeaking];
+}
 
 @end
