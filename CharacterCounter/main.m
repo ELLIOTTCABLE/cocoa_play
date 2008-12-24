deef67b4b860ce83663ffe95ef562ea7ca430b32
elliottcable <git@elliottcable.name>
2008-12-24 01:29:39 -0900

    New project from template, for the character counter challenge

---
+++ CharacterCounter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CharacterCounter
+//
+//  Created by elliottcable on 12/24/08.
+//  Copyright yreality 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
