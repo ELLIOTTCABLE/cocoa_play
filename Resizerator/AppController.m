8133ace25cdad13ef94362b70b2dbce4a75c98f6
elliottcable <git@elliottcable.name>
2008-12-25 00:47:05 -0900

    Implemented a resize locker-downer-thingie

--- Resizerator/AppController.m
+++ Resizerator/AppController.m
@@ -2,6 +2,12 @@
 
 @implementation AppController
 
-
+- (NSSize)windowWillResize:(NSWindow *)sender
+                    toSize:(NSSize)frameSize {
+  NSSize newSize;
+  newSize.width = frameSize.width;
+  newSize.height = 2 * frameSize.width;
+  return newSize;
+}
 
 @end
