2809dc1738cf6b8d5de57b323bafa0fc9afa2dd0
elliottcable <git@elliottcable.name>
2008-12-25 00:43:06 -0900

    Framework project for Chapter 6's first challenge

---
+++ Resizerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Resizerator
+//
+//  Created by elliottcable on 12/25/08.
+//  Copyright yreality 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
