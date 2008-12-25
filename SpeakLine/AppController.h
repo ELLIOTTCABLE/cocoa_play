c92d24cdfcde0fe9c3bc4c4614ff59e240b99ee1
elliottcable <git@elliottcable.name>
2008-12-24 22:57:57 -0900

    Added outlets for the start and stop buttons

--- SpeakLine/AppController.h
+++ SpeakLine/AppController.h
@@ -1,9 +1,13 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppController : NSObject {
+  IBOutlet NSButton *stopButton;
+  IBOutlet NSButton *startButton;
   IBOutlet NSTextField *textField;
   NSSpeechSynthesizer *speechSynth;
 }
+
 - (IBAction)sayIt:(id)sender;
 - (IBAction)stopIt:(id)sender;
+
 @end
