db47eb3bb612998664c3bd193228e86bd7dca4f7
elliottcable <git@elliottcable.name>
2008-12-28 11:12:55 -0900

    Initalizing the toDos array

--- ToDoList/AppController.m
+++ ToDoList/AppController.m
@@ -2,6 +2,14 @@
 
 @implementation AppController
 
+- (id)init {
+  [super init];
+  
+  toDos = [NSMutableArray arrayWithCapacity:1];
+  
+  return self;
+}
+
 - (void)controlTextDidEndEditing:(NSNotification *)aNotification {
 	NSLog(@"- controlTextDidEndEditing called");
 }
