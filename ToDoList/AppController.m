ae782773dd06e34051a510cdd347c90dbd751ce8
elliottcable <git@elliottcable.name>
2008-12-28 11:21:33 -0900

    Implemented the NSTableViewDataSource protocol in AppController

--- ToDoList/AppController.m
+++ ToDoList/AppController.m
@@ -10,6 +10,16 @@
   return self;
 }
 
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView {
+  return [toDos count];
+}
+
+- (id)                tableView:(NSTableView *)aTableView
+      objectValueForTableColumn:(NSTableColumn *)aTableColumn
+                            row:(NSInteger)rowIndex {
+  return [toDos objectAtIndex:rowIndex];
+}
+
 - (void)controlTextDidEndEditing:(NSNotification *)aNotification {
 	NSLog(@"- controlTextDidEndEditing called");
 }
