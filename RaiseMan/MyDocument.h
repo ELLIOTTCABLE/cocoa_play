0a48f977d272af50ef6339503af1d47f1ae2f9fe
elliottcable <git@elliottcable.name>
2008-12-29 12:52:03 -0500

    Cleaned up the pregenerated MyDocument files

--- RaiseMan/MyDocument.h
+++ RaiseMan/MyDocument.h
@@ -1,15 +1,7 @@
-//
-//  MyDocument.h
-//  RaiseMan
-//
-//  Created by elliott cable on 12/29/08.
-//  Copyright __MyCompanyName__ 2008 . All rights reserved.
-//
-
-
 #import <Cocoa/Cocoa.h>
 
-@interface MyDocument : NSDocument
-{
+@interface MyDocument : NSDocument {
+  
 }
+
 @end
