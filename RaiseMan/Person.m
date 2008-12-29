7fc46711dec4a26670225125dc85dc5b0157d0a9
elliottcable <git@elliottcable.name>
2008-12-29 12:46:08 -0500

    Implemented -init and -dealloc

--- RaiseMan/Person.m
+++ RaiseMan/Person.m
@@ -2,4 +2,19 @@
 
 @implementation Person
 
+- (id)init {
+  [super init];
+  expectedRaise = 5.0;
+  personName = @"New Person";
+  return self;
+}
+
+@synthesize personName;
+@synthesize expectedRaise;
+
+- (void)dealloc {
+  [personName release];
+  [super dealloc];
+}
+
 @end
