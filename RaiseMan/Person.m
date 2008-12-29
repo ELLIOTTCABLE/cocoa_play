e615053b7802b11dcb4647f26a0ba7d73c637236
elliottcable <git@elliottcable.name>
2008-12-29 14:23:22 -0500

    Added -setNilValueForKey:, to absolutely no effect (because the NSNumberFormatter will bitch at you)

--- RaiseMan/Person.m
+++ RaiseMan/Person.m
@@ -12,6 +12,14 @@
 @synthesize personName;
 @synthesize expectedRaise;
 
+- (void)setNilValueForKey:(NSString *)key {
+  if ([key isEqual:@"expectedRaise"]) {
+    [self setExpectedRaise:0.0];
+  } else {
+    [super setNilValueForKey:key];
+  }
+}
+
 - (void)dealloc {
   [personName release];
   [super dealloc];
