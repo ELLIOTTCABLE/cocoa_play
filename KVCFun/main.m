b772a6f0fb7f9ca7dd4b2c35b43dfaad836bb7cf
elliottcable <git@elliottcable.name>
2008-12-28 16:59:04 -0900

    New framework application for Chapter 7 (Key-Value Coding)

---
+++ KVCFun/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KVCFun
+//
+//  Created by elliott cable on 12/28/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
