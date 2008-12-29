0cb45a3c70fc6b7bc56995b549d52230038fb5d6
elliottcable <git@elliottcable.name>
2008-12-28 17:04:35 -0900

    Initialized 'fido' to 5 using Key-Value Coding mechanisms

--- KVCFun/AppController.m
+++ KVCFun/AppController.m
@@ -1,5 +1,11 @@
 #import "AppController.h"
 
 @implementation AppController
-
+- (id)init {
+  [super init];
+  [self setValue:[NSNumber numberWithInt:5] forKey:@"fido"];
+  NSNumber *n = [self valueForKey:@"fido"];
+  NSLog(@"fido = %@", n);
+  return self;
+}
 @end
