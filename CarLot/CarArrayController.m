61b6f3179f1d091e9bc466654ded662d67268311
elliottcable <git@elliottcable.name>
2008-12-30 17:45:32 -0500

    Over-rode newObject

--- CarLot/CarArrayController.m
+++ CarLot/CarArrayController.m
@@ -2,4 +2,11 @@
 
 @implementation CarArrayController
 
+- (id)newObject {
+  id newObj = [super newObject];
+  NSDate *now = [NSDate date];
+  [newObj setValue:now forKey:@"datePurchased"];
+  return newObj;
+}
+
 @end
