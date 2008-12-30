80c0e852ed25a3cb79d763d72ea17fc3a8e0c76e
elliottcable <git@elliottcable.name>
2008-12-30 16:33:18 -0500

    Created a new CoreData Document-Based Application framework for the CarLot exercise in chapter 11.

---
+++ CarLot/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  CarLot
+//
+//  Created by elliott cable on 12/30/08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ CarLot/MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  CarLot
+//
+//  Created by elliott cable on 12/30/08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
