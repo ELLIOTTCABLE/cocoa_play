0a3324dd0cf74100d6bf289fba31f0755a57d492
elliottcable <git@elliottcable.name>
2008-12-30 15:20:06 -0500

    Added header entries for custom observer-control methods

--- RaiseMan/MyDocument.h
+++ RaiseMan/MyDocument.h
@@ -7,4 +7,7 @@
 - (void)setEmployees:(NSMutableArray *)a;
 - (void)removeObjectFromEmployeesAtIndex:(int)index;
 - (void)insertObject:(Person *)p inEmployeesAtIndex:(int)index;
+
+- (void)startObservingPerson:(Person *)person;
+- (void)stopObservingPerson:(Person *)person;
 @end
