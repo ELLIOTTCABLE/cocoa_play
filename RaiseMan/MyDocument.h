f73a261e8dee8e2a5951013093233ac92fb209cb
elliottcable <git@elliottcable.name>
2008-12-30 15:34:57 -0500

    Added employeeController and tableView outlets, as well as a createEmployee action, and connected them all up

--- RaiseMan/MyDocument.h
+++ RaiseMan/MyDocument.h
@@ -3,7 +3,10 @@
 
 @interface MyDocument : NSDocument {
   NSMutableArray *employees;
+  IBOutlet NSTableView *tableView;
+  IBOutlet NSArrayController *employeeController;
 }
+- (IBAction)createEmployee:(id)sender;
 - (void)setEmployees:(NSMutableArray *)a;
 - (void)removeObjectFromEmployeesAtIndex:(int)index;
 - (void)insertObject:(Person *)p inEmployeesAtIndex:(int)index;
