b9c11febfe96d982068810b6b1eea5207cf4bce6
elliottcable <git@elliottcable.name>
2008-12-30 15:37:19 -0500

    Added implementation of createEmployee

--- RaiseMan/MyDocument.m
+++ RaiseMan/MyDocument.m
@@ -9,6 +9,47 @@
   return self;
 }
 
+- (IBAction)createEmployee:(id)sender {
+  NSWindow *w = [tableView window];
+  
+  // Try to end any editing that is taking place
+  BOOL editingEnded = [w makeFirstResponder:w];
+  if (!editingEnded) {
+    NSLog(@"Unable to end editing");
+    return;
+  }
+  NSUndoManager *undo = [self undoManager];
+  
+  // Has an edit occurred already in this event?
+  if ([undo groupingLevel]) {
+    // Close the last group
+    [undo endUndoGrouping];
+    // Open a new group
+    [undo beginUndoGrouping];
+  }
+  // Create the object
+  Person *p = [employeeController newObject];
+  
+  // Add it to the content array of 'employeeController'
+  [employeeController addObject:p];
+  [p release];
+  // Re-sort (in case the user has sorted a column)
+  [employeeController rearrangeObjects];
+  
+  // Get the sorted array
+  NSArray *a = [employeeController arrangedObjects];
+  
+  // Find the object just added
+  int row = [a indexOfObjectIdenticalTo:p];
+  NSLog(@"starting edit of %@ in row %d", p, row);
+  
+  // Begin the edit in the first column
+  [tableView editColumn:0
+                    row:row
+              withEvent:nil
+                 select:YES];
+}
+
 - (void)setEmployees:(NSMutableArray *)a {
   // This is an unusual setter method.  We are going to add a lot
   // of smarts to it in the next chapter.
