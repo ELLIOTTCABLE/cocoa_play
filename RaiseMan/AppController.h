095549edb84de7a92928894e69992e51a752fe07
elliottcable <git@elliottcable.name>
2008-12-31 11:31:18 -0500

    Adding a preferenceController

--- RaiseMan/AppController.h
+++ RaiseMan/AppController.h
@@ -1,7 +1,8 @@
 #import <Cocoa/Cocoa.h>
+@class PreferenceController;
 
 @interface AppController : NSObject {
-  
+  PreferenceController *preferenceController;
 }
-
+- (IBAction)showPreferencePanel:(id)sender;
 @end
