9ae8f78df5563a6e4c8c8c99e6f9e7340b91a33b
elliottcable <git@elliottcable.name>
2008-12-31 11:36:17 -0500

    Implemented the AppController

--- RaiseMan/AppController.m
+++ RaiseMan/AppController.m
@@ -1,5 +1,15 @@
 #import "AppController.h"
+#import "PreferenceController.h"
 
 @implementation AppController
+
+- (IBAction)showPreferencePanel:(id)sender{
+  // Is preferenceController nil?
+  if (!preferenceController) {
+    preferenceController = [[PreferenceController alloc] init];
+  }
+  NSLog(@"showing %@", preferenceController);
+  [preferenceController showWindow:self];
+}
 
 @end
