ff9742cccda572d1ae810631fcf3e7e7d0b48209
elliottcable <git@elliottcable.name>
2008-12-31 11:40:47 -0500

    Declared header for preference controller

--- RaiseMan/PreferenceController.h
+++ RaiseMan/PreferenceController.h
@@ -1,7 +1,9 @@
 #import <Cocoa/Cocoa.h>
 
 @interface PreferenceController : NSWindowController {
-  
+  IBOutlet NSColorWell *colorWell;
+  IBOutlet NSButton *checkbox;
 }
-
+- (IBAction)changeBackgroundColor:(id)sender;
+- (IBAction)changeNewEmptyDoc:(id)sender;
 @end
