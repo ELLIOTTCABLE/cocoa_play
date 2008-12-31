b6f1c1a43e26e7df4b3351eeb459edf52c16f0ce
elliottcable <git@elliottcable.name>
2008-12-31 12:03:01 -0500

    Implemented PreferenceController

--- RaiseMan/PreferenceController.m
+++ RaiseMan/PreferenceController.m
@@ -2,4 +2,24 @@
 
 @implementation PreferenceController
 
+- (id)init {
+  if (![super initWithWindowNibName:@"Preferences"])
+    return nil;
+  return self;
+}
+
+- (void)windowDidLoad {
+  NSLog(@"Nib file is loaded");
+}
+
+- (IBAction)changeBackgroundColor:(id)sender {
+  NSColor *color = [colorWell color];
+  NSLog(@"Color changed: %@", color);
+}
+
+- (IBAction)changeNewEmptyDoc:(id)sender {
+  int state = [checkbox state];
+  NSLog(@"Checkbox changed %d", state);
+}
+
 @end
