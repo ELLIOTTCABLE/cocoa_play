223caa1c080084ad7acd1607a9b6c20a2bb7176d
elliottcable <git@elliottcable.name>
2009-01-09 09:39:32 -0500

    Addressed some stylistic concerns in the default MyDocument class

--- OvalFun/MyDocument.h
+++ OvalFun/MyDocument.h
@@ -2,14 +2,11 @@
 //  MyDocument.h
 //  OvalFun
 //
-//  Created by elliott cable on 1/9/09.
-//  Copyright __MyCompanyName__ 2009 . All rights reserved.
-//
-
 
 #import <Cocoa/Cocoa.h>
 
-@interface MyDocument : NSDocument
-{
+@interface MyDocument : NSDocument {
+  
 }
+
 @end
--- OvalFun/MyDocument.m
+++ OvalFun/MyDocument.m
@@ -2,64 +2,32 @@
 //  MyDocument.m
 //  OvalFun
 //
-//  Created by elliott cable on 1/9/09.
-//  Copyright __MyCompanyName__ 2009 . All rights reserved.
-//
 
 #import "MyDocument.h"
 
 @implementation MyDocument
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-    
-        // Add your subclass-specific initialization here.
-        // If an error occurs here, send a [self release] message and return nil.
-    
-    }
-    return self;
+- (id)init {
+  if (![super init])
+    return nil;
+  return self;
 }
 
-- (NSString *)windowNibName
-{
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
-    return @"MyDocument";
+- (NSString *)windowNibName {
+  return @"MyDocument";
 }
 
-- (void)windowControllerDidLoadNib:(NSWindowController *) aController
-{
-    [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+- (void)windowControllerDidLoadNib:(NSWindowController *) aController {
+  [super windowControllerDidLoadNib:aController];
 }
 
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
-{
-    // Insert code here to write your document to data of the specified type. If the given outError != NULL, ensure that you set *outError when returning nil.
-
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-
-    // For applications targeted for Panther or earlier systems, you should use the deprecated API -dataRepresentationOfType:. In this case you can also choose to override -fileWrapperRepresentationOfType: or -writeToFile:ofType: instead.
-
-    if ( outError != NULL ) {
-		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-	}
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
+    if (outError) *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
 	return nil;
 }
 
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
-{
-    // Insert code here to read your document from the given data of the specified type.  If the given outError != NULL, ensure that you set *outError when returning NO.
-
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead. 
-    
-    // For applications targeted for Panther or earlier systems, you should use the deprecated API -loadDataRepresentation:ofType. In this case you can also choose to override -readFromFile:ofType: or -loadFileWrapperRepresentation:ofType: instead.
-    
-    if ( outError != NULL ) {
-		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-	}
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
+    if (outError) *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
     return YES;
 }
 
