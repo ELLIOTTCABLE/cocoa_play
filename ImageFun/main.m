77a8c04529f4480bc756d4d5f9619053e5f0f211
elliottcable <git@elliottcable.name>
2009-01-09 07:55:06 -0500

    Added a new application of framework "Cocoa Application"

---
+++ ImageFun/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ImageFun
+//
+//  Created by elliott cable on 1/9/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
