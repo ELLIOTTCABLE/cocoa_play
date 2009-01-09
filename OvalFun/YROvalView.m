fff6c044f5d507c095805c8e1725dc80c0f8aba8
elliottcable <git@elliottcable.name>
2009-01-09 10:15:39 -0500

    Added events handling, lots of logging, and pragma marks

--- OvalFun/YROvalView.m
+++ OvalFun/YROvalView.m
@@ -7,15 +7,35 @@
 
 @implementation YROvalView
 
+#pragma mark Hi, I'm YROvalView! Who're you?
+
 - (id)initWithFrame:(NSRect)frame {
+  NSLog(@"- initWithFrame:]");
   if (![super initWithFrame:frame])
     return nil;
   
   return self;
 }
 
+#pragma mark Things go clicky-click
+
+- (void)mouseDown:(NSEvent *)event {
+  NSLog(@"- mouseDown:%d]", [event clickCount]);
+}
+
+- (void)mouseDragged:(NSEvent *)event {
+  NSPoint p = [event locationInWindow];
+  NSLog(@"- mouseDragged:%@]", NSStringFromPoint(p));
+}
+
+- (void)mouseUp:(NSEvent *)event {
+  NSLog(@"- mouseUp:]");
+}
+
+#pragma mark Drawing shit
+
 - (void)drawRect:(NSRect)rect {
-  
+  NSLog(@"- drawRect:]");
 }
 
 @end
