9c4bdb41a4a9332914ffe7e133ef99940490ab01
elliottcable <git@elliottcable.name>
2009-01-09 08:45:25 -0500

    Implemented - showOpenPanel:

--- ImageFun/AppController.h
+++ ImageFun/AppController.h
@@ -4,6 +4,7 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "StretchView.h"
 
 @interface AppController : NSObject {
   IBOutlet StretchView *stretchView;
--- ImageFun/AppController.m
+++ ImageFun/AppController.m
@@ -7,4 +7,30 @@
 
 @implementation AppController
 
+- (void)openPanelDidEnd:(NSOpenPanel *)openPanel
+             returnCode:(int)returnCode
+            contextInfo:(void *)x {
+  // Did they choose "Open"?
+  if (returnCode == NSOKButton) {
+    NSString *path = [openPanel filename];
+    NSImage *image = [[NSImage alloc] initWithContentsOfFile:path];
+    [stretchView setImage:image];
+    [image release];
+  }
+}
+
+- (IBAction)showOpenPanel:(id)sender {
+  NSOpenPanel *panel = [NSOpenPanel openPanel];
+  
+  // Run the open panel
+  [panel beginSheetForDirectory:nil
+                           file:nil
+                          types:[NSImage imageFileTypes]
+                 modalForWindow:[stretchView window]
+                  modalDelegate:self
+                 didEndSelector:
+   @selector(openPanelDidEnd:returnCode:contextInfo:)
+                    contextInfo:NULL];
+}
+
 @end
