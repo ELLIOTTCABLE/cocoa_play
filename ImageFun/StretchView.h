91021c91a5828aff801af4b5564701b80a4ab329
elliottcable <git@elliottcable.name>
2009-01-09 07:57:41 -0500

    Added a StretchView NSView subclass

---
+++ ImageFun/StretchView.h
@@ -0,0 +1,12 @@
+//
+//  StretchView.h
+//  ImageFun
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface StretchView : NSView {
+  
+}
+
+@end
---
+++ ImageFun/StretchView.m
@@ -0,0 +1,20 @@
+//
+//  StretchView.m
+//  ImageFun
+//
+
+#import "StretchView.h"
+
+@implementation StretchView
+
+- (id)initWithFrame:(NSRect)frame {
+  if (![super initWithFrame:frame])
+    return nil;
+  return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+  // Drawing code here.
+}
+
+@end
