343bc2cb1e35a376972a144e4ea4d444bb445956
elliottcable <git@elliottcable.name>
2009-01-09 09:17:38 -0500

    Added a new "Cocoa Document-based Application" to play with ovals

---
+++ OvalFun/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  OvalFun
+//
+//  Created by elliott cable on 1/9/09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
