4fe144755f94803ea1b07c7356e9d36ddefd432c
elliottcable <git@elliottcable.name>
2009-01-10 07:46:41 -0500

    Added verbose logging

--- TypingTutor/BigLetterView.m
+++ TypingTutor/BigLetterView.m
@@ -8,13 +8,14 @@
 @implementation BigLetterView
 
 - (id)initWithFrame:(NSRect)frame {
+  NSLog(@"@BigLetterView - initWithFrame:]");
   if (![super initWithFrame:frame])
     return nil;
   return self;
 }
 
 - (void)drawRect:(NSRect)rect {
-  
+  NSLog(@"@BigLetterView - drawRect:]");
 }
 
 @end
