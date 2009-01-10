7c7dad9b5e6d55ed670128744a99bcf2de800a32
elliottcable <git@elliottcable.name>
2009-01-10 07:43:42 -0500

    Created a new project using the "Cocoa Project" template for chapter 19's "TypingTutor" example

---
+++ TypingTutor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TypingTutor
+//
+//  Created by elliott cable on 1/10/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
